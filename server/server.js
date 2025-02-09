4b9f468b35e23ad729a16618d1073e1f2b96d278
shondoe11 <shawnhadritan@gmail.com>
2025-02-09 16:33:17 +0800

    test add shawn branch

---
+++ server/server.js
@@ -0,0 +1,11 @@
+//imports
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+//config
+const app = express();
+mongoose.connect(process.env.MONGODB_URI)
+
+
