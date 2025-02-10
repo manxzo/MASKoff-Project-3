19f5bbe5be8000ed00f0d26c1b75167ea79f6563
shondoe11 <shawnhadritan@gmail.com>
2025-02-10 17:25:55 +0800

    pkg json

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,18 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from '@heroui/theme';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./index.html",
+    './index.html',
+    './src/**/*.{html,js}',
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [heroui()],
-}
+};
