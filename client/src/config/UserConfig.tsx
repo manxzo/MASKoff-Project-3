1034d5c4379cc0915754713f295357f7ccf2d916
manxzo <manzoball@gmail.com>
2025-02-15 19:00:48 +0800

    updated testing userLogin

---
+++ client/src/config/UserConfig.tsx
@@ -0,0 +1,69 @@
+import React, { createContext, useState, ReactNode } from "react";
+//Add new schema here
+
+interface Message {
+  id: string;
+  sender: string;
+  recipient: string;
+  messageContent: string;
+  timestamp: Date;
+}
+interface Chat{
+  id:string;
+  participants:string[];
+  messages:Message[];
+  createdAt:Date;
+  updatedAt:Date;
+  }
+  interface Friend {
+    id:string;
+    username:string;
+  }
+  interface User {
+    username:string;
+    id:string;
+    friends:Friend[];
+    friendRequests:Friend[];
+    chats:Chat[];
+  }
+
+//Update ConfigType here in this format -->  functionName:(argument:argumentType)=>returnType;
+interface UserConfigType {
+  user: User;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
+}
+//Dont touch
+export const UserConfigContext = createContext<UserConfigType | undefined>(
+  undefined
+);
+//Update your new declared function below
+export const UserConfigProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User>({
+  username:"",
+  id:"",
+  friends:[],
+  friendRequests:[],
+  chats:[]  
+  })
+ 
+
+//Update new function or config here
+
+  return (
+    <UserConfigContext.Provider
+      value={{ user, setUser }}//update the name of your function here
+    >
+      {children}
+    </UserConfigContext.Provider>
+  );
+};
+/*When u want use global variable anywhere else :
+import { useContext } from "react";
+import { ChatsConfigContext } from "@/config/ChatsConfig";
+
+inside the function or component u want to use:
+
+const chatContext = useContext(ChatsConfigContext);
+const {your function or object} = chatContext;
+
+*/
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,17 +4,17 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
-import { ChatsConfigProvider } from "./config/ChatsConfig.tsx";
+import { UserConfigProvider } from "./config/UserConfig.tsx";
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChatsConfigProvider>
+      <UserConfigProvider>
         <Provider>
           <App />
         </Provider>
-      </ChatsConfigProvider>
+      </UserConfigProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
